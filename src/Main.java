bc6fd818d5a4c26ad87badf54306d80e1f77defc
14kMaarqo <mareceksasinka3@seznam.cz>
2026-09-13 20:14:32 +0200

    Pridal jsem udaje

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+//takze o prazdninach jsem byl doma. Hral jsem hry nevim asi Minecraft. Delal jsem pres rpazdniny ridicak a dal jsem ho napoprve :D.Byl jsem na brigade a bylo hodne oslav. To je asi vse
+//Marek Šašinka - Uherský Brod - 18 - 676 796 911 - mujkrasnyemail123(zavinac)yahoo.cz
 }
